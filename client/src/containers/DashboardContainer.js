98b5dc47cdb6345b91fbc084c0843c0e1b218d10
Nick Mosher <nicholastmosher@gmail.com>
2017-03-24 16:11:00 -0400

    Bootstrap 4 loading with webpack.

--- client/src/containers/DashboardContainer.js
+++ client/src/containers/DashboardContainer.js
@@ -2,25 +2,19 @@
  * @author Nick Mosher <nicholastmosher@gmail.com>
  */
 import React from 'react';
-import { connect } from 'react-redux';
 import { Route } from 'react-router';
+import NavigationView from "../views/dashboard/NavigationView";
 import TransactionView from '../views/dashboard/TransactionView';
 import GraphsView from '../views/dashboard/GraphsView';
 import CollaboratorsView from '../views/dashboard/CollaboratorsView';
 
 const DashboardContainer = (props) => (
   <div>
-    <nav className="navbar budget-auth-navbar">
-      <h1 className="navbar-brand mb-0">{props.plan}</h1>
-    </nav>
+    <NavigationView />
     <Route path={props.match.url + "/:planId/transactions"} component={TransactionView}/>
     <Route path={props.match.url + "/:planId/graphs"} component={GraphsView}/>
     <Route path={props.match.url + "/:planId/collaborators"} component={CollaboratorsView}/>
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  plan: state.BudgetReducer.get('activePlan'),
-});
-
-export default connect(mapStateToProps)(DashboardContainer);
+export default DashboardContainer;
--- client/src/reducers/BudgetReducer.js
+++ client/src/reducers/BudgetReducer.js
@@ -5,18 +5,28 @@ import { Map } from 'immutable';
 import { ActionTypes } from '../constants/ActionTypes';
 
 let initialState = Map({
-  title: "Money Maid",
-  activePlan: "Personal Budget",
+  title: 'Money Maid',
+  activePlan: '0',
+  plans: Map({
+    '0': Map({
+      id: '0',
+      name: 'Personal Budget',
+    }),
+    '1': Map({
+      id: '1',
+      name: 'Business Budget',
+    })
+  }),
 });
 
 export default function Reducer(state = initialState, action) {
 
   switch(action.type) {
     case ActionTypes.SAY_HELLO:
-      return state.set('greeting', "Hello, " + action.name + "!");
+      return state.set('greeting', 'Hello, ' + action.name + '!');
 
     case ActionTypes.SAY_GOODBYE:
-      return state.set('greeting', "Goodbye " + action.name + "!");
+      return state.set('greeting', 'Goodbye ' + action.name + '!');
 
     default:
       return state;
