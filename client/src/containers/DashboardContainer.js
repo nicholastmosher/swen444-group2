b9fdf286a427b2bc052a7659f4beff2f5696a544
Daniel Roberts <djr9478@rit.edu>
2017-03-26 17:29:49 -0400

    Added basic dashboard page

--- client/src/containers/DashboardContainer.js
+++ client/src/containers/DashboardContainer.js
@@ -3,20 +3,19 @@
  */
 import React from 'react';
 import { Route } from 'react-router';
-import Map from '../reducers/BudgetReducer';
 import NavigationView from "../views/dashboard/NavigationView";
 import TransactionView from '../views/dashboard/TransactionView';
 import GraphsView from '../views/dashboard/GraphsView';
 import CollaboratorsView from '../views/dashboard/CollaboratorsView';
+import DashboardView from '../views/dashboard/DashboardView';
 
 const DashboardContainer = (props) => (
   <div>
     <NavigationView />
+    <Route path={props.match.url + "/dashboard"} component={DashboardView}/>
     <Route path={props.match.url + "/transactions"} component={TransactionView}/>
     <Route path={props.match.url + "/graphs"} component={GraphsView}/>
     <Route path={props.match.url + "/collaborators"} component={CollaboratorsView}/>
-
-    <h1> this is a very nice cool header </h1>
   </div>
 );
 
--- client/src/views/dashboard/NavigationView.js
+++ client/src/views/dashboard/NavigationView.js
@@ -16,7 +16,7 @@ const NavigationView = (props) => (
       <nav className="navbar budget-auth-navbar">
         <ul className="nav-inline">
           <li className="nav-inline-item">
-            <h1 className="navbar-brand mb-0">MoneyMaid</h1>
+              <NavLink to="/dashboard/dashboard" activeClassName="active"><h1 className="navbar-brand mb-0">MoneyMaid</h1></NavLink>
           </li>
           <li className="nav-inline-item">
             <div className="dropdown">
@@ -35,6 +35,9 @@ const NavigationView = (props) => (
               </div>
             </div>
           </li>
+            <li className="nav-inline-item">
+                <NavLink to="/dashboard/dashboard" activeClassName="active">Dashboard</NavLink>
+            </li>
           <li className="nav-inline-item">
             <NavLink to="/dashboard/transactions" activeClassName="active">Transactions</NavLink>
           </li>
