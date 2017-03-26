e143a1af32dbe72949609322fed94a0a2c5f8f7c
Daniel Roberts <djr9478@rit.edu>
2017-03-26 16:51:27 -0400

    moving to desktop computer, added some basic code for displaying a header on dashboard

--- client/src/containers/DashboardContainer.js
+++ client/src/containers/DashboardContainer.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { Route } from 'react-router';
+import Map from '../reducers/BudgetReducer';
 import NavigationView from "../views/dashboard/NavigationView";
 import TransactionView from '../views/dashboard/TransactionView';
 import GraphsView from '../views/dashboard/GraphsView';
@@ -14,6 +15,8 @@ const DashboardContainer = (props) => (
     <Route path={props.match.url + "/transactions"} component={TransactionView}/>
     <Route path={props.match.url + "/graphs"} component={GraphsView}/>
     <Route path={props.match.url + "/collaborators"} component={CollaboratorsView}/>
+
+    <h1> this is a very nice cool header </h1>
   </div>
 );
 
