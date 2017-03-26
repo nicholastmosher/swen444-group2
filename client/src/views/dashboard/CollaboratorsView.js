418e205ee07cf97d63df437bd95a97a969d4325c
Zachary Moran <zjm1065@rit.edu>
2017-03-25 20:24:17 -0400

    Added CollaboratorsView Stuff

--- client/src/views/dashboard/CollaboratorsView.js
+++ client/src/views/dashboard/CollaboratorsView.js
@@ -1,12 +1,59 @@
 /**
  * @author Nick Mosher <nicholastmosher@gmail.com>
+ * @author Zach Moran <zjm1065@rit.edu>
  */
 import React from 'react';
 
 const CollaboratorsView = (props) => (
-  <div>
-    <h1>CollaboratorsView</h1>
-  </div>
+    <div className="container-fluid">
+      <table className="table table-striped">
+        <thead>
+        <tr>
+          <th className="th">Username</th>
+          <th className="th">Name</th>
+          <th className="th">Email</th>
+          <th className="th">Access Privileges</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>JohnDoe12345</td>
+          <td>John Doe</td>
+          <td>john@example.com</td>
+          <td>Owner</td>
+        </tr>
+        <tr>
+          <td>MaryMoe54321</td>
+          <td>Mary Moe</td>
+          <td>mary@example.com</td>
+          <td>Editor
+            <button className="btn-danger btn-sm float-right">Revoke Access</button>
+          </td>
+        </tr>
+        <tr>
+          <td>JulyDooley98765</td>
+          <td>July Dooley</td>
+          <td>july@example.com</td>
+          <td>Viewer
+            <button className="btn-danger btn-sm float-right">Revoke Access</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <div className="row">
+        <h3 className="col-md-2">Add Collaborators</h3>
+        <div id="custom-search-input">
+          <div className="input-group">
+            <input type="text" className="search-query form-control" placeholder="Collaborator Username"/>
+            <span className="input-group-btn">
+              <button className="btn btn-success" type="button">
+                <span className=" glyphicon glyphicon-search">Add User</span>
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
 );
 
 export default CollaboratorsView;
--- client/src/views/dashboard/NavigationView.js
+++ client/src/views/dashboard/NavigationView.js
@@ -1,5 +1,6 @@
 /**
  * @author Nick Mosher <nicholastmosher@gmail.com>
+ * @author Zach Moran <zjm1065@rit.edu>
  */
 import React from 'react';
 import { connect } from 'react-redux';
@@ -9,32 +10,48 @@ import { NavLink } from 'react-router-dom';
  * Displays the navigation bar at the top of each "dashboard" page.
  */
 const NavigationView = (props) => (
-  <div>
-    <nav className="navbar budget-auth-navbar">
-      <ul className="nav navbar-nav">
-        <li className="nav-item">
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false">
-              {props.activePlan}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              {props.plans.map((plan, id) => (
-                <button className="dropdown-item">{plan.get('name')}</button>
-              ))}
+    <div>
+      <nav className="navbar budget-auth-navbar">
+        <ul className="nav-inline">
+          <li className="nav-inline-item">
+            <h1 className="navbar-brand mb-0">MoneyMaid</h1>
+          </li>
+          <li className="nav-inline-item">
+            <div className="dropdown">
+              <a className="dropdown-toggle"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false">
+                {props.activePlan}
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                {props.plans.map((plan, id) => (
+                    <a className="dropdown-item" href="/dashboard">{plan.get('name')}</a>
+                ))}
+              </div>
             </div>
-          </div>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/dashboard/transactions" activeClassName="active">Transactions</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </div>
+          </li>
+          <li className="nav-inline-item">
+            <NavLink to="/dashboard/transactions" activeClass="active">Transactions</NavLink>
+          </li>
+          <li className="nav-inline-item">
+            <NavLink to="/dashboard/graphs" activeClass="active">Graphs</NavLink>
+          </li>
+          <li className="nav-inline-item">
+            <NavLink to="/dashboard/collaborators" activeClass="active">Collaborators</NavLink>
+          </li>
+          <li className="nav-inline-item-right">
+            <li className="nav-inline-item">
+              <NavLink to="/dashboard/notifications" activeClass="active">Notifications</NavLink>
+            </li>
+            <li className="nav-inline-item">
+              <h1 className="navbar-brand mb-0">Username</h1>
+            </li>
+          </li>
+        </ul>
+      </nav>
+    </div>
 );
 
 const mapStateToProps = ({BudgetReducer}) => ({
