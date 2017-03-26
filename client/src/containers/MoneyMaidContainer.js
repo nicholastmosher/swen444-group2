85de0a97408f72ebbbd65ef5dc4cb79eae18e1df
Zachary Moran <zjm1065@rit.edu>
2017-03-26 19:33:09 -0400

    Added Splash Page to be the default url

---
+++ client/src/containers/MoneyMaidContainer.js
@@ -0,0 +1,23 @@
+/**
+ * @author Zachary Moran <zjm1065@rit.edu>
+ */
+import React from 'react';
+import { connect } from 'react-redux';
+import SplashView from '../views/moneymaid/SplashView';
+
+const MoneyMaidContainer = (props) => (
+    <div>
+      <nav className="navbar budget-auth-navbar">
+        <h1 className="navbar-brand mb-0">{props.title}</h1>
+      </nav>
+      <div>
+        <SplashView />
+      </div>
+    </div>
+);
+
+const mapStateToProps = (state) => ({
+  title: state.BudgetReducer.get('title'),
+});
+
+export default connect(mapStateToProps)(MoneyMaidContainer);
