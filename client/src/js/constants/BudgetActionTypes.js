0bacf85949336e801c8a9f7619c89fb6ce311325
Nick Mosher <nicholastmosher@gmail.com>
2017-03-31 16:32:23 -0400

    Moves dummy data to own file; Adds Records.

--- client/src/js/constants/BudgetActionTypes.js
+++ client/src/js/constants/BudgetActionTypes.js
@@ -2,5 +2,9 @@
  * @author Nick Mosher <nicholastmosher@gmail.com>
  */
 export const BudgetActionTypes = {
+  ADD_PLAN: 'ADD_PLAN',
   SELECT_PLAN: 'SELECT_PLAN',
+  ADD_TRANSACTION: 'ADD_TRANSACTION',
+  ADD_COLLABORATOR: 'ADD_COLLABORATOR',
+  REMOVE_COLLABORATOR: 'REMOVE_COLLABORATOR',
 };
