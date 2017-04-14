4857aef9425c4108fc1ea8fae6cd1dc5f3494a9b
Nick Mosher <nicholastmosher@gmail.com>
2017-04-14 15:51:18 -0400

    Splits up reducers and actions to orgnize application state.

---
+++ client/src/js/constants/TransactionActionTypes.js
@@ -0,0 +1,8 @@
+/**
+ * @author Nick Mosher <nicholastmosher@gmail.com>
+ */
+export const TransactionActionTypes = {
+  ADD_TRANSACTION: 'ADD_TRANSACTION',
+  REMOVE_TRANSACTION: 'REMOVE_TRANSACTION',
+  EDIT_TRANSACTION: 'EDIT_TRANSACTION',
+};
