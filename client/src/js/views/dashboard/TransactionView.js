2c2ba363ba871ba36fbea77daaf6dd78f6bb3799
Nick Mosher <nicholastmosher@gmail.com>
2017-04-17 13:22:41 -0400

    Clicking transactions loads them into details table.

--- client/src/js/views/dashboard/TransactionView.js
+++ client/src/js/views/dashboard/TransactionView.js
@@ -1,69 +1,105 @@
 /**
  * @author Nick Mosher <nicholastmosher@gmail.com>
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getTransactions, toCSV } from '../../data/Utils';
 import AddTransactionModal from './AddTransactionModal';
 
-const TransactionView = (props) => {
-  const tModal = 'addTransactionModal';
-  return (
-    <div className="container-fluid">
-      <div className="row add-transactions">
-        <div className="col-md-1"></div>
-        <h2 className="col-md-2">Transactions</h2>
-        <div className="col-md-6"></div>
-        <div className="col-md-2 text-right">
-          <button className="btn btn-success"
-                  type="button"
-                  data-toggle="modal"
-                  data-target={'#' + tModal}>
-            Add Transaction
-          </button>
-        </div>
-        <div className="col-md-1"></div>
-      </div>
-      <div className="row add-transactions">
-        <div className="col-md-1"></div>
-        <div className="col-md-5">
-          <table className="table table-hover">
-            <thead>
-            <tr>
-              <th className="th">Date</th>
-              <th className="th">Amount</th>
-              <th className="th">Balance</th>
-            </tr>
-            </thead>
-            <tbody>
-            {props.transactions.entrySeq().map(([t, amount]) => (
-              <tr key={t.id}>
-                <td>{t.date}</td>
-                <td>{(t.amount > 0 ? ('+' + t.amount) : t.amount)}</td>
-                <td>{amount}</td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="col-md-5">
-          <div className="container">
-            <h3>Selected Transaction</h3>
-            <ul className="list-group">
-              <li className="list-group-item">ID:</li>
-              <li className="list-group-item">Date:</li>
-              <li className="list-group-item">Amount:</li>
-              <li className="list-group-item">Category:</li>
-              <li className="list-group-item">Tags:</li>
-            </ul>
+class TransactionView extends Component {
+  constructor() {
+    super();
+  }
+
+  componentWillMount() {
+    this.state = {
+      selectedTid: this.props.baseTid,
+    };
+  }
+
+  selectTransaction = (id) => {
+    if (!id) return;
+    if (!this.props.transactions.some((v, k) => k.id === id)) return;
+    this.setState(state => ({selectedTid: id}));
+  };
+
+  render() {
+    const tModal = 'addTransactionModal';
+    return (
+      <div className="container-fluid">
+        <div className="row add-transactions">
+          <div className="col-md-1"></div>
+          <h2 className="col-md-2">Transactions</h2>
+          <div className="col-md-6"></div>
+          <div className="col-md-2 text-right">
+            <button className="btn btn-success"
+                    type="button"
+                    data-toggle="modal"
+                    data-target={'#' + tModal}>
+              Add Transaction
+            </button>
           </div>
           <div className="col-md-1"></div>
         </div>
+        <div className="row add-transactions">
+          <div className="col-md-1"></div>
+          <div className="col-md-5">
+            <table className="table table-hover">
+              <thead>
+              <tr>
+                <th className="th">Date</th>
+                <th className="th">Amount</th>
+                <th className="th">Balance</th>
+              </tr>
+              </thead>
+              <tbody>
+              {this.props.transactions.entrySeq().map(([t, amount]) => (
+                <tr key={t.id}
+                    onClick={() => this.selectTransaction(t.id)}>
+                  <td>{t.date}</td>
+                  <td>{(t.amount > 0 ? ('+' + t.amount) : t.amount)}</td>
+                  <td>{amount}</td>
+                </tr>
+              ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="col-md-5">
+            <div className="container">
+              <h3>Selected Transaction</h3>
+              <table className="table table-striped">
+                <tbody>
+                  <tr>
+                    <td>Description</td>
+                    <td>{this.props.tById(this.state.selectedTid).description}</td>
+                  </tr>
+                  <tr>
+                    <td>Date</td>
+                    <td>{this.props.tById(this.state.selectedTid).date}</td>
+                  </tr>
+                  <tr>
+                    <td>Amount</td>
+                    <td>{this.props.tById(this.state.selectedTid).amount}</td>
+                  </tr>
+                  <tr>
+                    <td>Category</td>
+                    <td>{this.props.tById(this.state.selectedTid).category}</td>
+                  </tr>
+                  <tr>
+                    <td>Tags</td>
+                    <td>{toCSV(this.props.tById(this.state.selectedTid).tags)}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="col-md-1"></div>
+          </div>
+        </div>
+        <AddTransactionModal modalId={tModal} baseTid={this.props.baseTid}/>
       </div>
-      <AddTransactionModal modalId={tModal} baseTid={props.baseTid}/>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = ({PlanReducer, TransactionReducer}) => {
   const planId = PlanReducer.get('activePlan');
@@ -73,6 +109,9 @@ const mapStateToProps = ({PlanReducer, TransactionReducer}) => {
     planId,
     baseTid: baseTransactionId,
     transactions,
+    tById: (id) => {
+      return transactions.findKey((v, k) => k.id === id);
+    }
   });
 };
 
