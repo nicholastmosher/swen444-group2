d8bba995e29083a98e9b2db77e6d688bf70cbb4e
Nick Mosher <nicholastmosher@gmail.com>
2017-04-24 12:58:28 -0400

    Restructures bootstrap imports.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,7 +6,6 @@ let BUILD_DIR = path.resolve(__dirname, 'public');
 
 let config = {
   entry: [
-    `bootstrap-loader/lib/bootstrap.loader?configFilePath=${__dirname}/bs4.yml!bootstrap-loader/no-op.js`,
     APP_DIR + '/js/index.js'
   ],
   output: {
@@ -41,17 +40,6 @@ let config = {
       "window.jQuery": "jquery",
       Tether: "tether",
       "window.Tether": "tether",
-      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-      Button: "exports-loader?Button!bootstrap/js/dist/button",
-      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
     }),
   ],
   devServer: {
