54289c8729863b5bce7999bedff5e7ebafc34531
Nick Mosher <nicholastmosher@gmail.com>
2017-05-07 16:21:13 -0400

    Fixes key problem with plan links.

--- client/src/js/views/dashboard/NavigationView.js
+++ client/src/js/views/dashboard/NavigationView.js
@@ -37,7 +37,7 @@ const NavigationView = (props) => (
                 New Plan
               </button>
               {props.plans.valueSeq().map((plan) => (
-                  <NavLink to="/dashboard" activeClassName="active"><NavigationPlanView key={plan.id} plan={plan} modalId="confirmDeletePlan"/></NavLink>
+                  <NavLink key={plan.id} to="/dashboard" activeClassName="active"><NavigationPlanView plan={plan} modalId="confirmDeletePlan"/></NavLink>
               ))}
             </div>
           </div>
